11fbef8864520f90237402aabad79edd782acfa9
Curinfly <1686814964@qq.com>
2021-01-27 21:37:04 +0800

    finish the header component of blog

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
 import App from 'next/app'
 import 'antd/dist/antd.css'
+import '../styles/pages/common.css'
+import '../styles/components/header.css'
+
 
 export default App
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-
+import Header from '../components/Header'
 
 export default function Home() {
   return (
@@ -8,7 +8,7 @@ export default function Home() {
         <title>Home</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
+      <Header></Header>
     </div>
   )
 }
